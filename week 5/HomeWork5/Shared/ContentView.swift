7d9d932dbb00a1e07f892b17a4ac1313dac38b0e
yz7720 <yz7720@nyu.edu>
2022-02-26 15:47:05 -0500

    week5

---
+++ week 5/HomeWork5/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by yiting zeng on 25/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week 5/HomeWork5/Shared/HomeWork5App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork5App.swift
+//  Shared
+//
+//  Created by yiting zeng on 25/2/22.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
