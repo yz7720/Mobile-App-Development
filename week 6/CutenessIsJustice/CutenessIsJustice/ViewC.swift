712e8041ece90a79fc575f945af6590d0c445b16
yz7720 <yz7720@nyu.edu>
2022-05-03 23:59:49 -0400

    Final

--- week 6/CutenessIsJustice/CutenessIsJustice/ViewC.swift
+++ week 6/CutenessIsJustice/CutenessIsJustice/ViewC.swift
@@ -9,18 +9,42 @@ import SwiftUI
 
 struct ViewC: View {
     
-    let profileLinkNames: [String] = ["Saved Articles", "Folowers", "Following"]
+    let profileLinkNames: [String] = ["Following", "Followers", "Log Out"]
     
     var body: some View {
+        
         NavigationView {
-            VStack {
+            ZStack {
+                Color.primary_color.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 0) {
                 ForEach(profileLinkNames, id: \.self) { profileLinkName in
-                    Text(profileLinkName)
-                        .font(.body)
+                    VStack(spacing: 0) {
+                        HStack {
+                            Text(profileLinkName)
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(Color.primary)
+                                .font(.system(size: 20))
+                        }
+                        .contentShape(Rectangle()) // Defining the shape of the HStack
+                        .padding(EdgeInsets(top: 17, leading: 21, bottom: 17, trailing: 21))
+                        Divider()
+                    }
                 }
             }
-            .navigationBarTitle("Devin Jameson")
+            .navigationBarTitle("Profile")
+            .navigationBarItems(
+                leading:
+                    Text("Premium Member")
+                    .font(.body)
+                    .foregroundColor(Color(.systemGray)),
+                trailing: // Add trailing view
+                Image("avatar")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())) // Clip the image to a circle
         }
+    }
     }
 
 struct ViewC_Previews: PreviewProvider {
